05a92c548194ce22f92130bef3e3754714e3d163
goodmanship <rio@lightning-bolt.com>
2012-02-29 10:58:02 -0800

    Fix for MSSQL Server.  Using rails 3.2.1, activerecord-sqlserver-adapter 3.2.1 and tiny_tds 0.5.1

--- lib/octopus/rails3/abstract_adapter.rb
+++ lib/octopus/rails3/abstract_adapter.rb
@@ -24,7 +24,17 @@ module Octopus
       end
 
       def octopus_shard
-        @config[:octopus_shard]
+  			# @config is not present if this method is called on an ActiveRecord:ConnectionAdapters::SQLServerAdapter object
+				# so rebuild it here as a local variable "config," as a workaround -Rio
+				config = begin
+					file_name = Octopus.directory() + "/config/shards.yml"
+					if File.exists? file_name
+						config ||= HashWithIndifferentAccess.new(YAML.load(ERB.new(File.open(file_name).read()).result))[Octopus.env()]
+					else
+						config ||= HashWithIndifferentAccess.new
+					end
+				end
+				config[:octopus_shard]
       end
 
       def initialize_with_octopus_shard(*args)
